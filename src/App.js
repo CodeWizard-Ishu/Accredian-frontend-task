95a6ea87f03a5d6dbb5397fb98f8588173f9dff8
Utkarsh <utkarshjaiswal537@gmail.com>
2024-07-18 00:59:32 +0530

    minor bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HeroSection from "./components/HeroSection";
-import ReferralFormModal from "./components/ReferralFormModal";
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer'
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
     <>
-      <div>
-        {/* <HeroSection onReferNow={handleOpen} />
-        <ReferralFormModal open={open} onClose={handleClose} /> */}
-      </div>
       <div>
       <BrowserRouter>
         <Navbar/>
